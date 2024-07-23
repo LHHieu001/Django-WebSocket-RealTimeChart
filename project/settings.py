40d4ae5d176d9e3b19dbdb073d64e6dffa818667
Hieu Le Hoang (Goatee) <lehoanghieu10a1@gmail.com>
2024-07-23 23:20:29 +0700

    Chartjs Update

--- project/settings.py
+++ project/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     
     'channels',
     'chart',
+    'django_browser_reload',
     
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -132,12 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL  = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,20 @@
-const ctx = document.getElementById('myChart');
-var graphData = {
-    type: 'line',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor:[
-            'rgba(73, 198, 230, 0.5)',
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {}
-  }
 
-var myChart = new Chart(ctx, graphData);
+const ctx = document.getElementById('myChart');
+
+    var graphData = {
+        type: 'line',
+        data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+        },
+        options: {}
+    }
+
+    var myChart = new Chart(ctx, graphData);
 
 
 var socket = new WebSocket('ws://localhost:8000/ws/chart/');
@@ -31,6 +30,12 @@ socket.onmessage = function(e){
     graphData.data.datasets[0].data = newGraphData;
     myChart.update();
 
-    document.getElementById('myText').innerText = djData.num;
+    document.getElementById('t1').innerText = djData.num;
+
+
 }
+
+
+
+
 
